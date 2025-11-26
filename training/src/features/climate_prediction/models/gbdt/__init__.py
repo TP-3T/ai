38fbaaec0d37434abce6a3e2bac4e52b5421019e
jpg157 <jasonlau5000@gmail.com>
2025-11-26 00:18:53 -0800

    removed temperature target from absolute dataset

--- training/src/features/climate_prediction/models/gbdt/__init__.py
+++ training/src/features/climate_prediction/models/gbdt/__init__.py
@@ -1,6 +1,7 @@
 """Main file of second iteration of climate prediction model using gbdt."""
 
-from features.climate_prediction.constants.climate_prediction_dataset_cols import FUTURE_GMSL_TARGET_COL_NAME, FUTURE_TEMP_TARGET_COL_NAME
+from features.climate_prediction.constants.climate_prediction_dataset_cols import FUTURE_GMSL_TARGET_COL_NAME
+# FUTURE_TEMP_TARGET_COL_NAME
 from features.climate_prediction.models.gbdt.gbdt_test import test_gbdt_model # type: ignore
 from features.climate_prediction.models.gbdt.gbdt_train_and_validate import train_and_validate_gbdt_model # type: ignore
 from features.climate_prediction.data_processing.process_and_train_test_split import process_and_train_test_split

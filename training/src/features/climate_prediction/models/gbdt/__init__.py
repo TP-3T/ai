1eef1e6b7298304d7a6dce7d1a655c0f5902d611
jpg157 <jasonlau5000@gmail.com>
2025-11-25 19:07:20 -0800

    add data processing step into gbdt model creation pipeline

--- training/src/features/climate_prediction/models/gbdt/__init__.py
+++ training/src/features/climate_prediction/models/gbdt/__init__.py
@@ -2,9 +2,11 @@
 
 from features.climate_prediction.models.gbdt.gbdt_test import test_gbdt_model # type: ignore
 from features.climate_prediction.models.gbdt.gbdt_train_and_validate import train_and_validate_gbdt_model # type: ignore
+from src.features.climate_prediction.data_processing.process_and_train_test_split import process_and_train_test_split
 from sklearn.pipeline import Pipeline
 
 def main():
+  process_and_train_test_split()
   gbdt_model: Pipeline = train_and_validate_gbdt_model()
   test_gbdt_model(gbdt_model)
 

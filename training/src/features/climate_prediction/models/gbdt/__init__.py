98381ba97b7ec453b95e4b3e33b9138a52984b7a
Jason L <jasonlau5000@gmail.com>
2025-11-25 19:24:54 -0800

    import fix

--- training/src/features/climate_prediction/models/gbdt/__init__.py
+++ training/src/features/climate_prediction/models/gbdt/__init__.py
@@ -2,7 +2,7 @@
 
 from features.climate_prediction.models.gbdt.gbdt_test import test_gbdt_model # type: ignore
 from features.climate_prediction.models.gbdt.gbdt_train_and_validate import train_and_validate_gbdt_model # type: ignore
-from src.features.climate_prediction.data_processing.process_and_train_test_split import process_and_train_test_split
+from features.climate_prediction.data_processing.process_and_train_test_split import process_and_train_test_split
 from sklearn.pipeline import Pipeline
 
 def main():
